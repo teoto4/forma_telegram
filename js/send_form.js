5480ed176a5cf569513d1d40fbbbbe9606cf139c
Revy <162366071+teoto4@users.noreply.github.com>
2024-08-09 16:56:42 +0300

    first commit

---
+++ js/send_form.js
@@ -0,0 +1,16 @@
+const token = "7494564803:AAHc8LdfUOSeqjOzXXrUQ0K-tqopp2p5z0U";
+const chat_id = "-1002236120894";
+const uri_api = `https://api.telegram.org/bot${token}/sendMessage`;
+
+document.querySelector("#tg").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let message = `<b>Заявка с сайта</b>\n`;
+  message += `<b>Отправитель: </b> ${this.name.value} \n`;
+  message += `<b>Почта: </b> ${this.email.value}`;
+
+  axios.post(uri_api, {
+    chat_id: chat_id,
+    parse_mode: "html",
+    text: message,
+  });
+});
